414530d6fd408aee06f930c4882de48c07f64077
Pluto Y <kuaileainits@163.com>
2015-11-14 14:15:15 +0800

    Create a new project for epam
    Create a new project for epam

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoToHell
+//
+//  Created by ChipSea on 15/11/14.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoToHell
+//
+//  Created by ChipSea on 15/11/14.
+//  Copyright © 2015年 pluto-y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
